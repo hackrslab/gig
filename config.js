efd27aa4ae526faf273975703d13b1c427dc6ee2
insanehong <insanehong@gmail.com>
2014-08-22 10:26:49 +0900

    Bump up version to v0.2.0

--- config.js
+++ config.js
@@ -12,7 +12,7 @@ var path = require('path');
 
 var config = {
   title : 'gig',
-  version : "0.1.17",
+  version : "0.2.0",
   _baseDir : __dirname,
   _libDir : path.join(__dirname,'lib'),
   _moduleDir : path.join(__dirname,'lib','gig'),
